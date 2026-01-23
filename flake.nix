65483d2a6ca53d758856b1b5ddf199bbe8909eaa
Shenmarukai <shanemulc@comcast.net>
2026-01-23 00:33:42 -0500

    Fix flake.

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-	description = "A mathematics programming language";
+	description = "An automated depth first llm supported reverse engineering tool";
 
 	inputs = {
 		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
@@ -9,19 +9,11 @@
 		pkgs = nixpkgs.legacyPackages."x86_64-linux";
 	in {
 		devShells."x86_64-linux".default = let
-			llvm = pkgs.llvmPackages.llvm;
-			llvmConfig = pkgs.llvmPackages.llvm.dev;
 		in pkgs.mkShell {
 			buildInputs = with pkgs; [
 				cargo rustc rustfmt clippy rust-analyzer
-				llvm
-				llvmPackages.clang
-				llvmPackages.bintools
-				libxml2
 			];
 
-			LLVM_SYS_210_PREFIX = "${llvm}";
-			LLVM_CONFIG_PATH = "${llvmConfig}/bin/llvm-config";
 			env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 		};
 	};
